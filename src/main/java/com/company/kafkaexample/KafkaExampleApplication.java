7155c26d02db09e6481705bd8312256197e699b9
decagon <godsonjonathan39@gmail.com>
2022-05-03 15:49:58 +0100

    configured kafka config class

--- src/main/java/com/company/kafkaexample/KafkaExampleApplication.java
+++ src/main/java/com/company/kafkaexample/KafkaExampleApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KafkaExampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(KafkaExampleApplication.class, args);
+
+         SpringApplication.run(KafkaExampleApplication.class, args);
     }
 
 }
